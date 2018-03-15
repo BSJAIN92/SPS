0f673ac178716f7803cf66327bee03004d0be9cb
Konrad Kleeberger <git@konrad.online>
2018-03-15 12:05:43 +0100

    updated normal dist file

--- matlab/script.m
+++ matlab/script.m
@@ -1,10 +1,13 @@
 % load data
-processed = readtable("processed.csv")
+processed = readtable("processed.csv", 'Delimiter',';')
 
 % get file handler
 fileID = fopen("distribution.csv","w")
 
 arrayfun(@(x) magic(x, processed, fileID, "C12"), string(unique(processed(processed.Cell == "C12", :).BSSID)),'UniformOutput',false)
+arrayfun(@(x) magic(x, processed, fileID, "C13"), string(unique(processed(processed.Cell == "C13", :).BSSID)),'UniformOutput',false)
+arrayfun(@(x) magic(x, processed, fileID, "C14"), string(unique(processed(processed.Cell == "C14", :).BSSID)),'UniformOutput',false)
+arrayfun(@(x) magic(x, processed, fileID, "C15"), string(unique(processed(processed.Cell == "C15", :).BSSID)),'UniformOutput',false)
 
 % close the file
 fclose(fileID)
